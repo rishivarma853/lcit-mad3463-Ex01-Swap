4e14a46be5a288bd54eb6eb18e269a1f0f5c5b6f
Rishi Varma Kalidindi <rishivarma.lc.dev@gmail.com>
2022-11-09 18:43:04 -0500

    Project Done

---
+++ swap/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\n  Enter x :  ");
+        int x = scan.nextInt();
+        System.out.print("\n  Enter y :  ");
+        int y = scan.nextInt();
+
+        System.out.println("\n  Before Swapping : \t x = " + x + ", y = " + y);
+
+        x = x + y;
+        y = x - y;
+        x = x - y;
+
+        System.out.println("\n  After Swapping : \t\t x = " + x + ", y = " + y);
+
+    }
+}
